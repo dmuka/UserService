c8bc00dde628d0e17d0d7a7a461b018ff8493f1f
Dmitry Belousov <belavusdm@gmail.com>
2025-01-28 20:10:05 +0300

    Add user and role aggregates

---
+++ src/Domain/Users/Role.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain.Users;
+
+public class Role
+{
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public ICollection<User> Users { get; private set; }
+
+    public Role(string name)
+    {
+        Name = name;
+        Users = new List<User>();
+    }
+        
+    public void AddUser(User user)
+    {
+        Users.Add(user);
+    }
+}
--- src/Domain/Users/User.cs
+++ src/Domain/Users/User.cs
@@ -1,10 +1,63 @@
-﻿namespace Domain.Users;
+﻿using Domain.ValueObjects;
+
+namespace Domain.Users;
 
 public class User : Entity
 {
     public int Id { get; set; }
-    public required string Email { get; set; }
-    public required string FirstName { get; set; }
-    public required string LastName { get; set; }
-    public required string PasswordHash { get; set; }
+    public string Username { get; private set; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public PasswordHash PasswordHash { get; private set; }
+    public Email Email { get; private set; }
+    public int RoleId { get; private set; }
+    public Role Role { get; private set; }
+
+    public User() { }
+
+    public User(
+        string userName,
+        string firstName,
+        string lastName,
+        PasswordHash passwordHash, 
+        Email email, 
+        Role role)
+    {
+        if (string.IsNullOrWhiteSpace(userName))
+            throw new ArgumentException("Username can't be null or empty.", nameof(userName));
+        if (string.IsNullOrWhiteSpace(firstName))
+            throw new ArgumentException("First name can't be null or empty.", nameof(firstName));
+        if (string.IsNullOrWhiteSpace(lastName))
+            throw new ArgumentException("Last name can't be null or empty.", nameof(lastName));
+
+        Username = userName;
+        FirstName = firstName;
+        LastName = lastName;
+        PasswordHash = passwordHash ?? throw new ArgumentNullException(nameof(passwordHash));
+        Email = email ?? throw new ArgumentNullException(nameof(email));
+        Role = role ?? throw new ArgumentNullException(nameof(role));
+        RoleId = role.Id;
+    }
+
+    public void ChangeEmail(Email newEmail)
+    {
+        ArgumentNullException.ThrowIfNull(newEmail);
+
+        Email = newEmail;
+    }
+
+    public void ChangePassword(PasswordHash newPasswordHash)
+    {
+        ArgumentNullException.ThrowIfNull(newPasswordHash);
+
+        PasswordHash = newPasswordHash;
+    }
+
+    public void ChangeRole(Role newRole)
+    {
+        ArgumentNullException.ThrowIfNull(newRole);
+
+        Role = newRole;
+        RoleId = newRole.Id;
+    }
 }
