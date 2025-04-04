18fbdb93275d62ba960cd95c0691ce21d428314b
Dmitry Belousov <belavusdm@gmail.com>
2025-04-04 15:23:50 +0300

    Add adding of a new user

---
+++ src/WebApi/Pages/Users/Add.cshtml.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Application.Users.SignUp;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApi.Pages.Users;
+
+public class CreateModel(ISender sender) : PageModel
+{
+    [BindProperty] 
+    public InputModel Input { get; set; } = new();
+    
+    public class InputModel
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+        [Display(Name = "User name")]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; } = string.Empty;
+        
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; } = string.Empty;
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+    
+    public IActionResult OnGet()
+    {
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        var command = new SignUpUserCommand(Input.UserName, Input.Email, Input.FirstName, Input.LastName, Input.Password);
+        var result = await sender.Send(command);
+
+        if (result.IsFailure) return Page();
+
+        return RedirectToPage("./Index");
+    }
+}
