644aa441bc46a743bebf664845b0fef88e09f2db
Dmitry Belousov <belavusdm@gmail.com>
2025-04-04 14:32:56 +0300

    Add full list users page

---
+++ src/WebApi/Pages/Users/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using Application.Users.GetAll;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApi.Pages.Users;
+
+public class IndexModel(ISender sender) : PageModel
+{
+    public IList<UserResponse> Users { get; set; } = [];
+
+    [BindProperty(SupportsGet = true)]
+    public string SearchString { get; set; } = string.Empty;
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        var query = new GetAllUsersQuery();
+        var result = await sender.Send(query);
+        
+        if (result.IsFailure) return Page();
+        
+        var users = result.Value;
+
+        if (!string.IsNullOrEmpty(SearchString))
+        {
+            users = users.Where(user => 
+                user.FirstName.Contains(SearchString) || 
+                user.LastName.Contains(SearchString) ||
+                user.Email.Contains(SearchString));
+        }
+        
+        Users = users.ToList();
+
+        return Page();
+    }
+}
