a0e9534de8d355bf615f20868956f7f59a3fa771
Dmitry Belousov <belavusdm@gmail.com>
2025-04-10 23:06:08 +0300

    Add command and handler for user update

---
+++ src/Application/Users/Update/UpdateUserCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using Application.Abstractions.Authentication;
+using Application.Abstractions.Messaging;
+using Core;
+using Domain.Roles;
+using Domain.Users;
+
+namespace Application.Users.Update;
+
+public class UpdateUserCommandHandler(
+    IUserRepository repository, 
+    IUserRoleRepository userRoleRepository,
+    IUserContext userContext) 
+    : ICommandHandler<UpdateUserCommand, int>
+{
+    public async Task<Result<int>> Handle(
+        UpdateUserCommand command, 
+        CancellationToken cancellationToken)
+    {
+        if (userContext.UserRole != "Admin")
+        {
+            return Result.Failure<int>(UserErrors.Unauthorized());
+        }
+        
+        await repository.UpdateUserAsync(command.User, cancellationToken);
+        
+        var roleIds = command.User.RoleIds.Select(roleId => roleId.Value);
+        var result =await userRoleRepository.UpdateUserRolesAsync(command.User.Id.Value, roleIds, cancellationToken);
+
+        return result;
+    }
+}
