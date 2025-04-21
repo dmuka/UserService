f8a86369c15a0eb955f09752d6e955cd2bd53a68
Dmitry Belousov <belavusdm@gmail.com>
2025-04-21 22:54:03 +0300

    Add pagination

---
+++ src/WebApi/Pages/PagedResult.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApi.Pages;
+
+public class PagedResult<T>
+{
+    public PagedResult()
+    {
+        TotalPages = (int)Math.Ceiling(TotalItems / (double)PageSize);
+    }
+
+    public IList<T> Items { get; set; } = [];
+    public int TotalItems { get; set; }
+    public int PageSize { get; set; }
+    public int PageNumber { get; set; }
+    public int TotalPages { get; set; }
+}
--- src/WebApi/Pages/Users/Index.cshtml.cs
+++ src/WebApi/Pages/Users/Index.cshtml.cs
@@ -9,13 +9,18 @@ namespace WebApi.Pages.Users;
 [Authorize(Policy = "UserManagementPolicy")]
 public class IndexModel(ISender sender) : PageModel
 {
+    public PagedResult<UserResponse> PagedData { get; set; }
+    public int CurrentPage { get; set; } = 1;
+    public int PageSize { get; set; } = 2;
     public IList<UserResponse> Users { get; set; } = [];
 
     [BindProperty(SupportsGet = true)]
     public string SearchString { get; set; } = string.Empty;
 
-    public async Task<IActionResult> OnGetAsync()
+    public async Task<IActionResult> OnGetAsync(int? pageNumber)
     {
+        CurrentPage = pageNumber ?? 1;
+        
         var query = new GetAllUsersQuery();
         var result = await sender.Send(query);
         
@@ -25,13 +30,26 @@ public class IndexModel(ISender sender) : PageModel
 
         if (!string.IsNullOrEmpty(SearchString))
         {
-            users = users.Where(user => 
-                user.FirstName.Contains(SearchString) || 
-                user.LastName.Contains(SearchString) ||
-                user.Email.Contains(SearchString));
+            users = users
+                .Where(user =>
+                    user.FirstName.Contains(SearchString) ||
+                    user.LastName.Contains(SearchString) ||
+                    user.Email.Contains(SearchString));
         }
-        
-        Users = users.ToList();
+
+        var userResponses = users as UserResponse[] ?? users.ToArray();
+        Users = userResponses
+            .Skip((CurrentPage - 1) * PageSize)
+            .Take(PageSize)
+            .ToList();
+
+        PagedData = new PagedResult<UserResponse>()
+        {
+            Items = Users,
+            PageNumber = CurrentPage,
+            PageSize = PageSize,
+            TotalItems = userResponses.Length
+        };
 
         return Page();
     }
