e35ec2ae3574b7c160e09c3e0e00c880950348d3
Dmitry Belousov <belavusdm@gmail.com>
2025-05-28 12:23:28 +0300

    Add kafka event publisher and it's settings

---
+++ src/Application/Abstractions/Kafka/IEventPublisher.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Abstractions.Kafka;
+
+public interface IEventPublisher
+{
+    Task PublishAsync<T>(string topic, T @event);
+}
---
+++ src/Infrastructure/Kafka/EventPublisher.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+using Application.Abstractions.Kafka;
+using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Kafka;
+
+public class EventPublisher(IConfiguration configuration) : IEventPublisher
+{
+    private readonly IProducer<Null, string> _producer = new ProducerBuilder<Null, string>(new ProducerConfig { BootstrapServers = configuration["Kafka:BootstrapServers"] }).Build();
+    private readonly string _topicPrefix = configuration["Kafka:TopicPrefix"] ?? throw new ProduceException<Null, string>(new Error(ErrorCode.TopicException), new DeliveryResult<Null, string>());
+
+    public async Task PublishAsync<T>(string topic, T @event)
+    {
+        var message = JsonSerializer.Serialize(@event);
+        await _producer.ProduceAsync($"{_topicPrefix}{topic}", new Message<Null, string> { Value = message });
+    }
+
+    public void Dispose()
+    {
+        _producer?.Dispose();
+    }
+}
