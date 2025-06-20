40596d335d206fe1346daafc06f19a84fad2d9f1
Dmitry Belousov <belavusdm@gmail.com>
2025-06-20 20:08:55 +0300

    Add RoleAuthorizationHandlerTests

---
+++ tests/UserService.Infrastructure.Tests/Authorization/RoleAuthorizationHandlerTests.cs
@@ -0,0 +1,103 @@
+﻿using System.Security.Claims;
+using Infrastructure.Authorization;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+
+namespace UserService.Infrastructure.Tests.Authorization;
+
+[TestFixture]
+public class RoleAuthorizationHandlerTests
+{
+    private const string Admin = "Admin";
+    private const string User = "User";
+    private const string SuperAdmin = "SuperAdmin";
+    
+    private const string MockAuthType = "MockAuthType";
+    
+    private ClaimsPrincipal _adminPrincipalMock;
+    private RolesAuthorizationRequirement[] _adminAuthorizationRequirements;
+    private RolesAuthorizationRequirement[] _multipleRolesAuthorizationRequirements;
+    
+    private RoleAuthorizationHandler _handler;
+
+    [SetUp]
+    public void Setup()
+    {
+        _adminPrincipalMock = new ClaimsPrincipal(new ClaimsIdentity(
+        [
+            new Claim(ClaimTypes.Role, Admin)
+        ], MockAuthType));
+        
+        _adminAuthorizationRequirements = [new RolesAuthorizationRequirement([Admin])];
+        _multipleRolesAuthorizationRequirements = [new RolesAuthorizationRequirement([Admin, SuperAdmin])];
+        
+        _handler = new RoleAuthorizationHandler();
+    }
+    
+    [TestCase(true, Admin, Admin, Description = "User has required role")]
+    [TestCase(false, User, Admin, Description = "User does not have required role")]
+    [TestCase(false, null, Admin, Description = "User is not authenticated")]
+    public async Task HandleRequirementAsync_Authorization_ShouldBehaveCorrectly(
+        bool expectedSuccess, 
+        string? userRole, 
+        string requiredRole)
+    {
+        // Arrange
+        var principal = userRole == null 
+            ? new ClaimsPrincipal(new ClaimsIdentity()) 
+            : new ClaimsPrincipal(new ClaimsIdentity([new Claim(ClaimTypes.Role, userRole)], MockAuthType));
+        var context = new AuthorizationHandlerContext(_adminAuthorizationRequirements, principal, null);
+
+        // Act
+        await _handler.HandleAsync(context);
+
+        // Assert
+        Assert.That(context.HasSucceeded, Is.EqualTo(expectedSuccess));
+    }
+    
+    [Test]
+    public async Task HandleRequirementAsync_UserHasOneOfMultipleRequiredRoles_ShouldSucceed()
+    {
+        // Arrange
+        var context = new AuthorizationHandlerContext(_multipleRolesAuthorizationRequirements, _adminPrincipalMock, null);
+
+        // Act
+        await _handler.HandleAsync(context);
+
+        // Assert
+        Assert.That(context.HasSucceeded, Is.True);
+    }
+
+    [Test]
+    public async Task HandleRequirementAsync_UserHasMultipleRoles_ShouldSucceed()
+    {
+        // Arrange
+        var multiRolePrincipal = new ClaimsPrincipal(new ClaimsIdentity(
+        [
+            new Claim(ClaimTypes.Role, User),
+            new Claim(ClaimTypes.Role, Admin)
+        ], MockAuthType));
+    
+        var context = new AuthorizationHandlerContext(_adminAuthorizationRequirements, multiRolePrincipal, null);
+
+        // Act
+        await _handler.HandleAsync(context);
+
+        // Assert
+        Assert.That(context.HasSucceeded, Is.True);
+    }
+
+    [Test]
+    public async Task HandleRequirementAsync_UserIsNotAuthenticated_ShouldNotSucceed()
+    {
+        // Arrange
+        var user = new ClaimsPrincipal(new ClaimsIdentity());
+        var context = new AuthorizationHandlerContext(_adminAuthorizationRequirements, user, null);
+
+        // Act
+        await _handler.HandleAsync(context);
+
+        // Assert
+        Assert.That(context.HasSucceeded, Is.False);
+    }
+}
